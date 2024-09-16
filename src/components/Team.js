719d758ec95e6f86df1e9fef793c16a0e89b9512
Anjuluthra13 <122430165+Anjuluthra13@users.noreply.github.com>
2024-09-16 17:03:48 +0530

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Team.css'; 
 import Sudhir from '../image/Sudhir.jpeg'; 
 import Kamal from '../image/Kamal.jpeg'; 
 import Anju from '../image/Anju.jpeg';
@@ -8,30 +8,30 @@ import { FaLinkedin, FaGithub, FaLink } from 'react-icons/fa';
 
 const Card = ({ team }) => {
   return (
-    <div className="card">
-      <div className="poster">
+    <div className="teamcard">
+      <div className="teamposter">
         <img src={team.poster} alt={team.title} />
       </div>
-      <div className="details">
+      <div className="teamdetails">
         <h1>{team.title}</h1>
         <h2>{team.position}</h2>
-        <div className="rating">
+        <div className="teamrating">
           {Array.from({ length: 5 }, (_, index) => (
             <i
               key={index}
-              className={`fas fa-star${index < team.rating ? '' : '-half-alt'}`}
+              className={fas fa-star${index < team.rating ? '' : '-half-alt'}}
               style={{ color: index < team.rating ? '#e3c414' : '' }}
             />
           ))}
           <span>{team.rating}/5</span>
         </div>
-        <div className="tags">
+        <div className="teamtags">
           {team.tags.map((tag, index) => (
-            <span key={index} className={`tag${tag.color ? ' ' + tag.color : ''}`}>{tag.text}</span>
+            <span key={index} className={teamtag${tag.color ? ' ' + tag.color : ''}}>{tag.text}</span>
           ))}
         </div>
-        <p className="desc">{team.desc}</p>
-        <div className="social-links">
+        <p className="teamdesc">{team.desc}</p>
+        <div className="teamsocial-links">
           <h3>Social Links</h3>
           <ul>
             {team.socialLinks.map((link, index) => (
@@ -84,22 +84,7 @@ const Team = () => {
         { url: 'https://anjuluthra13.github.io/em-1-project', icon: 'fa-link' } 
       ]
     },
-    {
-      poster: Kamal, 
-      title: 'Kamal',
-      position: 'Member',
-      rating: 4,
-      tags: [
-        { text: 'Frontend' },
-        { text: 'Backend' }
-      ],
-      desc: 'An aspiring designer with a flair for creativity and a keen eye for detail.',
-      socialLinks: [
-        { url: 'https://www.linkedin.com/in/kamal-sharma-828a5018b/', icon: 'fa-linkedin' },
-        { url: 'https://github.com/kamal8996', icon: 'fa-github' },
-        { url: 'https://github.com/kamal8996/portfolio', icon: 'fa-link' } 
-      ]
-    },
+    
     {
       poster: Sandhya,
       title: 'Sandhya Prajapati',
@@ -116,14 +101,29 @@ const Team = () => {
         { url: 'https://prajapatisan.github.io/creative-cv/', icon: 'fa-link' } 
       ]
     },
-   
+    {
+      poster: Kamal, 
+      title: 'Kamal',
+      position: 'Member',
+      rating: 4,
+      tags: [
+        { text: 'Frontend' },
+        { text: 'Backend' }
+      ],
+      desc: 'An aspiring designer with a flair for creativity and a keen eye for detail.',
+      socialLinks: [
+        { url: 'https://www.linkedin.com/in/kamal-sharma-828a5018b/', icon: 'fa-linkedin' },
+        { url: 'https://github.com/kamal8996', icon: 'fa-github' },
+        { url: 'https://github.com/kamal8996/portfolio', icon: 'fa-link' } 
+      ]
+    }
   ];
 
   return (
-    <div className="team-page">
+    <div className="teampage">
       <h1>Meet Our Team - Work4You</h1>
       <p>We are a dedicated group of professionals committed to delivering the best solutions for your business.</p>
-      <div className="wrapper">
+      <div className="teamwrapper">
         {team.map((member, index) => (
           <Card key={index} team={member} />
         ))}
