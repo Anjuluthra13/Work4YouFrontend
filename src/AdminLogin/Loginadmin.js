f4d66dfd698e1e4ead30254e9fb7d7434300c4ff
Anjuluthra13 <122430165+Anjuluthra13@users.noreply.github.com>
2024-09-16 17:49:48 +0530

    Deploy

--- src/AdminLogin/Loginadmin.js
+++ src/AdminLogin/Loginadmin.js
@@ -12,76 +12,27 @@ const Loginadmin = () => {
     
     const loginAdmin = async (e) => {
         e.preventDefault();
-
-        const res = await fetch('http://localhost:8080/admin/signin' ,{
-        method:"POST",
-        headers:{
-            "Content-Type" : "application/json"
-
-        },
-        body:JSON.stringify({
-            email,
-            password
-        })
-
-       }); 
-
-    const data = res.json();
-    if (res.status === 400 || !data) {
-        window.alert("Invalid  Credentials");
-   
-    } else {
-  
-        dispatchs({type:"admin", payload:true })
-        window.alert("Login Successfull");
-        history.push("/admin");
+    
+        const res = await fetch('https://work4youbackend-production.up.railway.app/admin/signin', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+    
+        const data = await res.json(); // Ensure you await the response
+    
+        if (res.status === 400 || !data.token) {
+            window.alert("Invalid Credentials");
+        } else {
+            localStorage.setItem("jwtToken", data.token); // Store the token
+            dispatchs({ type: "admin", payload: true });
+            window.alert("Login Successful");
+            history.push("/admin");
+        }
     }
-
-
-
-    }
-
-
-
-  return (
-    <>
-              <section className="contactus-section " style={{ marginTop: "14rem" , marginBottom:"14rem"}}>
-                <div className="container" >
-                    <div className="row card-5" style={{margin:"0rem 2rem 0rem 2rem" , padding:"2rem 2rem 2rem 2rem"}}>
-                    <div className="col-md-6 col-lg-6">
-                           
-                            <h1 style={{fontWeight:"bold"}}>Login Admin</h1>
-                             <br></br>
-                              
-                             <form method="POST">
-                                <div className="input-container" style={{marginTop:'1.5rem'}}>
-                                    <i className="fa fa-envelope icon">  </i>
-                                    <input className="input-field" type="email" placeholder="Email" name="email" id="email" autoComplete='off' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                                </div>
-
-                             
-                                <div className="input-container"  style={{marginTop:'1.5rem'}}>
-                                    <i className="fa fa-key icon" ></i>
-                                    <input className="input-field" type="password" placeholder="Password" name="password"  id="password" autoComplete='off' value={password} onChange={(e) => setPassword(e.target.value)}/>
-                                </div>
-                             
-
-                                <Link type="submit" className="btn btn-dark"   id="signin" value="login" onClick={loginAdmin} style={{marginTop:"2rem"}}>Login</Link>
-                            </form>
-
-                        </div>
-                        <div className="col-md-6  col-lg-6  " style={{padding:"4rem 4rem 4rem 4rem"}} >
-                        
-                          <center> <img src={image} className="img-fluid" style={{width:"300px"}}>
-                            </img></center> 
-                          {/* <center>  <Link to="/registerlogin" style={{textDecoration:"none"}} > Create a account</Link></center> */}
-                        </div>
-                       
-                    </div>
-                </div>
-            </section>
-    </>
-  )
-}
-
-export default Loginadmin
+    
